e62736bc9187926de5098dc524c3687d9ac8213c
Omar Ibrahim <os1991ai@gmail.com>
2022-11-30 18:41:02 +0800

    Implement merge sort

--- SortingAlgorithms/SortingCSharp.cs
+++ SortingAlgorithms/SortingCSharp.cs
@@ -11,14 +11,17 @@ using SortingAlgorithms;
 int[] arr1 = new int[] { 5, 2, 4, 1, 9, 8 };
 int[] arr2 = new int[] { 5, 2, 4, 1, 9, 8 };
 int[] arr3 = new int[] { 5, 2, 4, 1, 9, 8 };
+int[] arr4 = new int[] { 5, 2, 4, 1, 9, 8 };
 
 SelectionSort.Sort(arr1);
 InsertionSort.Sort(arr2);
 BubbleSort.Sort(arr3);
+MergeSort.Sort(arr4);
 
 ConsoleUtils.DisplayArray(arr1);
 ConsoleUtils.DisplayArray(arr2);
 ConsoleUtils.DisplayArray(arr3);
+ConsoleUtils.DisplayArray(arr4);
 
 namespace SortingAlgorithms
 {
@@ -119,6 +122,47 @@ namespace SortingAlgorithms
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
+        }
+    }
+
+    public static class MergeSort
+    {
+        public static void Sort(int[] arr)
+        {
+            int[] aux = new int[arr.Length];
+
+            Sort(arr, aux, 0, arr.Length-1);
+        }
+
+        private static void Sort(int[] arr, int[] aux, int lo, int hi)
+        {
+            if (hi <= lo)
+                return;
+
+            int mid = lo + (hi - lo) / 2;
+            Sort(arr, aux, lo, mid);
+            Sort(arr, aux, mid + 1, hi);
+            Merge(arr, aux, lo, mid, hi);
+        }
+
+        private static void Merge(int[] arr, int[] aux, int lo, int mid, int hi)
+        {
+            for (int k = lo; k <= hi; k++)
+                aux[k] = arr[k];
+
+            int i = lo, j = mid + 1;
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid) arr[k] = aux[j++];
+                else if (j > hi) arr[k] = aux[i++];
+                else if (IsLess(aux[j], aux[i])) arr[k] = aux[j++];
+                else arr[k] = aux[i++];
+            }
+        }
+
+        private static bool IsLess(IComparable n1, IComparable n2)
+        {
+            return n1.CompareTo(n2) < 0;
         }
     }
 
